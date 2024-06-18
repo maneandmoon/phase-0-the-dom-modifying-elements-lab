27d36ceaccbfcfc315f822db8b85badfd69ac17d
LindaTa <lindata@maneandmoon.com>
2024-06-18 12:05:52 -0500

    dom mod ele lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = 'victory'
+
+const newContent = document.createTextNode("Linda is the champion")
+
+newHeader.appendChild(newContent);
+
+const currentHeader = document.getElementById("victory");
+    document.body.insertBefore(newHeader, currentHeader);
+
